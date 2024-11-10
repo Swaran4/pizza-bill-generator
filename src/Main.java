d6d347699a72de9bc984e5ff227fcc0e82f8d9c3
dipanc1 <dipanchhabra@gmail.com>
2024-11-10 10:21:08 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args) {
+
+//           Pizza basePizza = new Pizza(true);
+//           basePizza.addExtraCheese();
+//           basePizza.addExtraToppings();
+//           basePizza.takeAway();
+//           basePizza.getBill();
+
+
+        DeluxePizza dp = new DeluxePizza(true);
+        dp.addExtraCheese();
+        dp.addExtraToppings();
+        dp.takeAway();
+        dp.getBill();
+
+    }
+}
